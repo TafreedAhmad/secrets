d70bb3f4c2b2607b644170b74439c2d3a106e3cd
Tafreed ahmad <you@example.com>
2022-08-02 22:38:40 +0500

    Finally done using OAuth20 for google sign in

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 const app = express();
@@ -14,6 +16,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 //////Setting the express session
 app.use(session({
     secret: "Our little secret",
@@ -34,10 +39,12 @@ mongoose.connect("mongodb://localhost:27017/userDb", { useNewUrlParser: true, us
 ///Schema
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 
 /////MODEL
 const User = new mongoose.model("User", userSchema);
@@ -45,21 +52,65 @@ const User = new mongoose.model("User", userSchema);
 
 ////Serializing and deserializing
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, {
+            id: user.id,
+            username: user.username,
+        });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+
+
+////Adding the google authentications 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+
+            return cb(err, user);
+        });
+    }
+));
 
 
 
 app.get("/", function (req, res) {
     res.render("Home");
 });
+
+app.get("/auth/google", passport.authenticate('google', {
+
+    scope: ["profile"]
+
+}));
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect("/secrets");
+    });
+
 app.get("/login", function (req, res) {
     res.render("Login");
 });
 
-app.get("/logout", function(req,res){
-    req.logout(function(err){
-        if(err){
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) {
             return next(err);
         }
     });
@@ -71,13 +122,59 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+
+    //// $ne= not equal to null
+    User.find({ "secrets": { $ne: null } }, function(err, foundUsers){
+        if (err) {
+            console.log(err);            
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {userWithSecrets : foundUsers});
+                                                
+            }
+            
+        }
+    })
+})
+
+app.get("/submit", function (req, res) {
+
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     }
     else {
         res.redirect("login");
     }
 })
+
+
+app.post("/submit", function (req, res) {
+
+    const submittedSecret = req.body.secret;
+
+    // console.log(user);
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.redirect("secrets");
+                    }
+                })
+            }
+
+        }
+    })
+
+
+});
+
+
 
 app.post("/register", function (req, res) {
 
@@ -88,7 +185,7 @@ app.post("/register", function (req, res) {
         }
         else {
             passport.authenticate("local")(req, res, function () {
-                console.log(user, 101);
+                console.log(user);
                 res.redirect("/secrets");
             });
 
@@ -104,46 +201,6 @@ app.post("/login", passport.authenticate("local", {
     failureRedirect: "/login",
 }));
 
-
-///////////////BETTER EXPLANATION
-
-// app.post("/login", function (req, res) {
-//     //check the DB to see if the username that was used to login exists in the DB
-//     User.findOne({ username: req.body.username }, function (err, foundUser) {
-//         //if username is found in the database, create an object called "user" that will store the username and password
-//         //that was used to login
-//         if (foundUser) {
-//             const user = new User({
-//                 username: req.body.username,
-//                 password: req.body.password
-//             });
-//             //use the "user" object that was just created to check against the username and password in the database
-//             //in this case below, "user" will either return a "false" boolean value if it doesn't match, or it will
-//             //return the user found in the database
-//             passport.authenticate("local", function (err, user) {
-//                 if (err) {
-//                     console.log(err);
-//                 } else {
-//                     //this is the "user" returned from the passport.authenticate callback, which will be either
-//                     //a false boolean value if no it didn't match the username and password or
-//                     //a the user that was found, which would make it a truthy statement
-//                     if (user) {
-//                         //if true, then log the user in, else redirect to login page
-//                         req.login(user, function (err) {
-//                             res.redirect("/secrets");
-//                         });
-//                     } else {
-//                         res.redirect("/login");
-//                     }
-//                 }
-//             })(req, res);
-//             //if no username is found at all, redirect to login page.
-//         } else {
-//             //user does not exists
-//             res.redirect("/login")
-//         }
-//     });
-// });
 
 
 
