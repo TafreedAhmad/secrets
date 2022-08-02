0f68bb84e1551105093a46e29547dd938ae4fcfb
Tafreed ahmad <you@example.com>
2022-08-02 06:08:16 +0500

    Before the google authentication, ADDing cookies and sessions

--- app.js
+++ app.js
@@ -3,31 +3,52 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
-// const md5=require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+//////Setting the express session
+app.use(session({
+    secret: "Our little secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+////Connect to DB
 mongoose.connect("mongodb://localhost:27017/userDb", { useNewUrlParser: true, useUnifiedTopology: true });
 
+///////DB SETUP
+///Schema
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+userSchema.plugin(passportLocalMongoose);
 
-// console.log(process.env.SECRET);
 
-
-//No need of doing it if we have made hash password usign md5, we just use md5 where we take the password from the user.
-// userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
-
+/////MODEL
 const User = new mongoose.model("User", userSchema);
+
+
+////Serializing and deserializing
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get("/", function (req, res) {
     res.render("Home");
@@ -35,60 +56,94 @@ app.get("/", function (req, res) {
 app.get("/login", function (req, res) {
     res.render("Login");
 });
+
+app.get("/logout", function(req,res){
+    req.logout(function(err){
+        if(err){
+            return next(err);
+        }
+    });
+    res.redirect("/");
+});
+
 app.get("/register", function (req, res) {
     res.render("Register");
 });
 
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    }
+    else {
+        res.redirect("login");
+    }
+})
+
 app.post("/register", function (req, res) {
 
-
-    ///THis will save the user password as in the form of hashh
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            // password: md5(req.body.password)///for md5
-            password: hash
-        });
-        newUser.save(function (err) {
-            if (!err) {
-                res.render("secrets");
-            }
-            else {
-                console.log(err);
-            }
-        });
-    });
-});
-
-app.post("/login", function (req, res) {
-    const username = req.body.username;
-    // const password = md5(req.body.password);//For md5
-    const password = req.body.password;
-
-    User.findOne({ email: username }, function (err, foundOne) {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
-            res.send(err);
-        } else {
-            // if (foundOne.password === password) {
-            //     res.render("secrets");
-            // }
-            if (foundOne) {
-
-                bcrypt.compare(password, foundOne.password, function (err, result) {
-                    //result == true
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                    //console.log(result);
-                });
-            }
+            console.log(err);
+            res.redirect("/register");
         }
-    });
+        else {
+            passport.authenticate("local")(req, res, function () {
+                console.log(user, 101);
+                res.redirect("/secrets");
+            });
+
+        }
+    })
+
+
 });
 
+// Handle 'POST' requests made on the '/login' route:
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+}));
 
 
+///////////////BETTER EXPLANATION
+
+// app.post("/login", function (req, res) {
+//     //check the DB to see if the username that was used to login exists in the DB
+//     User.findOne({ username: req.body.username }, function (err, foundUser) {
+//         //if username is found in the database, create an object called "user" that will store the username and password
+//         //that was used to login
+//         if (foundUser) {
+//             const user = new User({
+//                 username: req.body.username,
+//                 password: req.body.password
+//             });
+//             //use the "user" object that was just created to check against the username and password in the database
+//             //in this case below, "user" will either return a "false" boolean value if it doesn't match, or it will
+//             //return the user found in the database
+//             passport.authenticate("local", function (err, user) {
+//                 if (err) {
+//                     console.log(err);
+//                 } else {
+//                     //this is the "user" returned from the passport.authenticate callback, which will be either
+//                     //a false boolean value if no it didn't match the username and password or
+//                     //a the user that was found, which would make it a truthy statement
+//                     if (user) {
+//                         //if true, then log the user in, else redirect to login page
+//                         req.login(user, function (err) {
+//                             res.redirect("/secrets");
+//                         });
+//                     } else {
+//                         res.redirect("/login");
+//                     }
+//                 }
+//             })(req, res);
+//             //if no username is found at all, redirect to login page.
+//         } else {
+//             //user does not exists
+//             res.redirect("/login")
+//         }
+//     });
+// });
 
 
 
